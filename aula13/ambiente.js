6bbb1ca8c008f025efa3f0b161e2f9ec7c3a0951
CarlosHenrique191 <carloshr.171@gmail.com>
2023-03-30 11:03:33 -0300

    aula 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var contador = 1
+//while
+/*while (contador <= 6) {
+    console.log('passo ' + contador)
+    contador++
+}*/
+//do while
+do {
+    console.log('passo ' + contador)
+    contador++
+} while (contador <= 6)
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
