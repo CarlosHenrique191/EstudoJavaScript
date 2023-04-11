ef3c80aeb2281b586e8f99734c4df282c8afae87
CarlosHenrique191 <carloshr.171@gmail.com>
2023-04-11 08:57:43 -0300

    ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,16 @@
-alert('Oie')
+function verificar() {
+    let numtab = document.getElementById('numTab')
+    let seltab = document.getElementById('selTab')
+    
+    if (Number(numtab.value) == 0) {
+        alert('0 é invalido, por favor digite outro numero')
+    }else{
+        let n = Number(numtab.value)
+        seltab.innerHTML = ''
+        for(i=1;i<=10;i++) {
+            let item = document.createElement('option')
+            item.text = n + 'x' + i + '= ' + (n*i)
+            seltab.appendChild(item)
+        }
+    }
+}
